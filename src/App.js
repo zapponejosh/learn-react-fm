ef731fa570a433829fc86b5898a45d3925236070
Josh Zappone <zappone.josh@gmail.com>
2020-10-16 14:12:44 -0700

    commit point2: tooling

--- src/App.js
+++ src/App.js
@@ -9,20 +9,21 @@ const Pet = ({ name, animal, breed }) => {
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt me!"),
-    React.createElement(Pet, { 
-      name: "Tex", 
-      animal: "Dog", 
-      breed: "CutiePie" 
+    React.createElement(Pet, {
+      name: "Tex",
+      animal: "Dog",
+      breed: "CutiePie",
     }),
-    React.createElement(Pet, { 
-      name: "Goldilocks", 
-      animal: "Fish", 
-      breed: "Goldfish" 
+    React.createElement(Pet, {
+      name: "Goldilocks",
+      animal: "Fish",
+      breed: "Goldfish",
     }),
-    React.createElement(Pet, { 
-      name: "JigglyPuff", 
-      animal: "Gineau Pig", 
-      breed: "Round" }),
+    React.createElement(Pet, {
+      name: "JigglyPuff",
+      animal: "Gineau Pig",
+      breed: "Round",
+    }),
   ]);
 };
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
